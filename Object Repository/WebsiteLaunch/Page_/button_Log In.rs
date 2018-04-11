9e08fdcdffafa85792927e9ede81bae731760446
ngupte <ngupte@10.96.8.37>
2018-04-11 17:03:48 -0500

    Launch OneKey Website - Record and Playback

---
+++ Object Repository/WebsiteLaunch/Page_/button_Log In.rs
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Log In</name>
+   <tag></tag>
+   <elementGuidId>c05e3685-85ab-4414-9743-9056df3116a9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;modal-login__form&quot;)/div[3]/button[@class=&quot;btn btn--primary-dark-filled&quot;][count(. | //button[@type = 'submit' and (text() = 'Log In' or . = 'Log In')]) = count(//button[@type = 'submit' and (text() = 'Log In' or . = 'Log In')])]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn--primary-dark-filled</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log In</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;modal-login__form&quot;)/div[3]/button[@class=&quot;btn btn--primary-dark-filled&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
